a99ca143b2952f37a405a024b4a2e22f09e27c35
StephanieCampbell <stephanie.campbell4@mail.mcgill.ca>
2012-11-15 21:50:13 -0500

    Added registration

--- Registration.php
+++ Registration.php
@@ -3,8 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-
-<title>EWEEK :: HOME</title>
+		<title>CLUB :: About</title>
 
 <?php include("SetUp.php");?>
 
@@ -14,19 +13,21 @@
 
 		<?php include("Header.php");?>
 
-		<div id="content">
+
+<div id="content">
 			<div id="contentpage">
 				<div class="innertube">
-					<h1>Welcome!</h1>
-						
-						<p>Sincerely,</p>
-						<p>EWEEK Committee</p>
+					<h1>EWEEK Registration Page</h1>
+                                        <iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dGo3d3dNbFB1dWhmSUJCbDRTeFdfNWc6MQ" width="760" height="625" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                                        **Please note that this does not reserve your spot! You still need to bring 60$ cash to your department head / team captain!
+
 				</div>
 			</div>
 		</div>
 
 		<?php include("Footer.php");?>
-		
+
 	</div>
+
 </body>
 </html>
